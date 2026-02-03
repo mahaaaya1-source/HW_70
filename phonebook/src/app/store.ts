2d8d0fe58d3a55b3119d33a311caff1dad555dce
mahaaaya1-source <mahaaaya1@gmail.com>
2026-02-03 14:05:58 +0600

    chore: finalize phonebook project

--- phonebook/src/app/store.ts
+++ phonebook/src/app/store.ts
@@ -7,5 +7,6 @@ export const store = configureStore({
   },
 });
 
+// Типы можно экспортировать из types.ts
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
